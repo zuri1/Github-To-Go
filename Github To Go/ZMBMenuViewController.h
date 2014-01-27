f975378205f3b854c53aa99fdd38e8f019bf0738
zuri1 <zuri.biringer@gmail.com>
2014-01-27 14:45:27 -0800

    Initial commit

---
+++ Github To Go/ZMBMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZMBMenuViewController.h
+//  Github To Go
+//
+//  Created by Zuri Biringer on 1/27/14.
+//  Copyright (c) 2014 Zuri Biringer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZMBMenuViewController : UITableViewController
+
+@end
